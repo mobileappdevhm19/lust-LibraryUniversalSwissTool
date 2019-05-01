53c46b84f2aa5121adb71e9acf7f2076140e41be
Felix Strobel <mail@felixstrobel.de>
2019-05-01 17:42:11 +0200

    add capacity widgets to capacity

--- lust/lib/views/capacity.dart
+++ lust/lib/views/capacity.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
-import 'package:lust/models/capacity/percentPerHour.dart';
+import 'package:lust/widgets/capacityGraph.dart';
+import 'package:lust/widgets/capacityInfo.dart';
 
 class CapacityWidget extends StatefulWidget {
   @override
@@ -8,65 +8,22 @@ class CapacityWidget extends StatefulWidget {
 }
 
 class _CapacityWidgetState extends State<CapacityWidget> {
-
   final _title = "Capacity";
-
-  List<charts.Series<PercentPerHour, String>> _createChartData() {
-    final data = [
-      new PercentPerHour("10", 8),
-      new PercentPerHour("20", 9),
-      new PercentPerHour("30", 10),
-      new PercentPerHour("40", 11),
-      new PercentPerHour("50", 12),
-      new PercentPerHour("60", 13),
-      new PercentPerHour("70", 14),
-      new PercentPerHour("80", 15),
-      new PercentPerHour("90", 16),
-      new PercentPerHour("100", 17),
-      new PercentPerHour("90", 18),
-      new PercentPerHour("80", 19),
-      new PercentPerHour("70", 20),
-      new PercentPerHour("60", 21),
-      new PercentPerHour("50", 22),
-      new PercentPerHour("40", 23),
-    ];
-
-    return [
-      new charts.Series<PercentPerHour, String>(
-          id: 'Statistics',
-          domainFn: (PercentPerHour rolls, _) => rolls.percent,
-          measureFn: (PercentPerHour rolls, _) => rolls.hour,
-          data: data,
-          // Set a label accessor to control the text of the bar label.
-          labelAccessorFn: (PercentPerHour rolls, _) =>
-          '${rolls.percent.toString()}: ${rolls.hour.toString()}\%')
-    ];
-  }
 
   @override
   Widget build(BuildContext context) {
-
-    var chart = new charts.BarChart(
-      _createChartData(),
-      animate: true,
-      vertical: false,
-      barRendererDecorator: new charts.BarLabelDecorator<String>(),
-      // Hide domain axis.
-      domainAxis:
-      new charts.OrdinalAxisSpec(renderSpec: new charts.NoneRenderSpec()),
-    )
-
-
-
     return Scaffold(
         appBar: AppBar(
           title: Text(_title),
         ),
         body: Center(
-          child: Text('M'),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+               CapacityGraph.withSampleData(),
+//                CapacityInfo(),
+              ]),
         ));
   } // build
-
-
 
 }
