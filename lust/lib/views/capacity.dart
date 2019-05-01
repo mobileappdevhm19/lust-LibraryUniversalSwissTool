50c831efcdc9a151bd931d4afb14d742f0c64e74
andre <brauni.andre@gmail.com>
2019-05-01 18:20:31 +0200

    repair capacity

--- lust/lib/views/capacity.dart
+++ lust/lib/views/capacity.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lust/widgets/capacityGraph.dart';
 import 'package:lust/widgets/capacityInfo.dart';
 
 class CapacityWidget extends StatefulWidget {
