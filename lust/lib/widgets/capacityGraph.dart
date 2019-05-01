a3b676472c01db029f3ae986387ccd294e852151
Felix Strobel <mail@felixstrobel.de>
2019-05-01 18:15:09 +0200

    add widget for capacity graph

---
+++ lust/lib/widgets/capacityGraph.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:lust/models/capacity/percentPerHour.dart';
+
+
+class CapacityGraph extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  CapacityGraph(this.seriesList, {this.animate});
+
+  factory CapacityGraph.withSampleData() {
+    return new CapacityGraph(
+      _createChartData(),
+      // Disable animations for image tests.
+      animate: true,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.BarChart(
+      seriesList,
+      animate: animate,
+    );
+  }
+
+  static List<charts.Series<PercentPerHour, String>> _createChartData() {
+    final data = [
+      new PercentPerHour(10, "8"),
+      new PercentPerHour(20, "9"),
+      new PercentPerHour(30, "10"),
+      new PercentPerHour(40, "11"),
+      new PercentPerHour(50, "12"),
+      new PercentPerHour(60, "13"),
+      new PercentPerHour(70, "14"),
+      new PercentPerHour(80, "15"),
+      new PercentPerHour(90, "16"),
+      new PercentPerHour(100, "17"),
+      new PercentPerHour(90, "18"),
+      new PercentPerHour(80, "19"),
+      new PercentPerHour(70, "20"),
+      new PercentPerHour(60, "21"),
+      new PercentPerHour(50, "22"),
+      new PercentPerHour(40, "23"),
+    ];
+
+    return [
+      new charts.Series<PercentPerHour, String>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (PercentPerHour percent, _) => percent.hour,
+        measureFn: (PercentPerHour percent, _) => percent.percent,
+        data: data,
+      )
+    ];
+  }
+}
+
