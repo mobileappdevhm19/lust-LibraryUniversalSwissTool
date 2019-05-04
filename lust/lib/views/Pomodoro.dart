5504ddd6eb07fe39fc01d2f02a6fd11cf7e297bd
mike <lauterba@hm.edu>
2019-05-04 15:10:40 +0200

    included table for description of the parameter

--- lust/lib/views/Pomodoro.dart
+++ lust/lib/views/Pomodoro.dart
@@ -26,7 +26,8 @@ class _PomodoroState extends State<Pomodoro> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
-                  height: _height*0.60,
+                  height: _height*0.40,
+                  child: promDesc(),
                 ),
                 Container(
                   alignment: Alignment.center,
@@ -35,5 +36,74 @@ class _PomodoroState extends State<Pomodoro> {
               ]),
         ));
   } // build
+
+}
+
+
+Widget promDesc(){
+  return new Container(
+    child: Column(
+      //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        new Row(
+          children: <Widget>[
+            const Expanded(child: Text(
+              "period:",
+              textScaleFactor: 0.8,
+              style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
+              textAlign: TextAlign.center,
+            )),
+            const Expanded(child: Text(
+              "25 min",
+              textScaleFactor: 0.8,
+              style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
+              textAlign: TextAlign.center,
+            )),
+          ],
+        ),
+        new Divider(
+          height: 50.0,
+          indent: 0.0,
+          color: Colors.black,
+        ),
+        new Row(
+          children: <Widget>[
+            const Expanded(child: Text(
+              "break:",
+              textScaleFactor: 0.8,
+              style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
+              textAlign: TextAlign.center,
+            )),
+            const Expanded(child: Text(
+              "5 / 15 min",
+              textScaleFactor: 0.8,
+              style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
+              textAlign: TextAlign.center,
+            )),
+          ],
+        ),
+        new Divider(
+          height: 50.0,
+          indent: 0.0,
+          color: Colors.black,
+        ),
+        new Row(
+          children: <Widget>[
+            const Expanded(child: Text(
+              "periods:",
+              textScaleFactor: 0.8,
+              style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
+              textAlign: TextAlign.center,
+            )),
+            const Expanded(child: Text(
+              "4",
+              textScaleFactor: 0.8,
+              style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
+              textAlign: TextAlign.center,
+            )),
+          ],
+        ),
+    ]),
+  );
 
 }
