d500047f7c76f186292ca259d3c4ca7949fbee92
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-05-06 19:52:40 +0200

    working, pending: style details

--- lust/lib/views/checkinPage.dart
+++ lust/lib/views/checkinPage.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-<<<<<<< HEAD
 import 'package:lust/widgets/widgetCreator.dart';
-=======
-// import 'package:lust/widgets/widgetCreator.dart'; // commented out because of revert #71
->>>>>>> a3c481a07447757cd5482eb46ba3e064dd895a1f
 import 'package:lust/widgets/getTextBox.dart';
 
 class CheckInOut extends StatefulWidget {
@@ -25,34 +21,36 @@ class _CheckInOutState extends State<CheckInOut> {
     print("INICIO + $_textButton");
     return Scaffold(
         appBar: AppBar(title: Text(_title)),
-<<<<<<< HEAD
         drawer: WidgetCreator.getDrawer(context),
-=======
         //drawer: WidgetCreator.getDrawer(context), // commented out because of revert PR #71
->>>>>>> a3c481a07447757cd5482eb46ba3e064dd895a1f
         body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            TextBox.getTextBox(context, "Subject", "Good luck with that!"),
-            TextBox.getTextBox(context, "Time goal", "Be realistic!"),
-            TextBox.getTextBox(context, "Locker number",
-               "I know that is difficult to remember"),
-            InkWell(
-                onTap: onButtonPressed,
-                child: Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(150),
-                        color: _colorButton),
-                    child: Center(
-                        child: Text(
-                      _textButton,
-                      style: TextStyle(fontSize: 28),
-                    ))))
-          ],
-        )));
+            child: Padding(
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    TextBox.getTextBox(
+                        context, "Subject", "Good luck with that!"),
+                    TextBox.getTextBox(context, "Time goal", "Be realistic!"),
+                    TextBox.getTextBox(context, "Locker number",
+                        "I know that is difficult to remember"),
+                    Padding(
+                      padding: EdgeInsets.only(top: 60),
+                        child: InkWell(
+                            onTap: onButtonPressed,
+                            child: Container(
+                                height: 150,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(150),
+                                    color: _colorButton),
+                                child: Center(
+                                    child: Text(
+                                  _textButton,
+                                  style: TextStyle(fontSize: 28),
+                                )))))
+                  ],
+                ))));
   }
 
   void onButtonPressed() {
