dca2f8f19a0071d729114239eb5f33c5dbb502d4
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-05-07 10:38:13 +0200

    added icons

--- lust/lib/widgets/getTextBox.dart
+++ lust/lib/widgets/getTextBox.dart
@@ -6,35 +6,22 @@ class TextBox {
   //final String _text;
   //String inputUser;
 
-  static Widget getTextBox(BuildContext context, String _title, String _hint) {
+  static Widget getTextBox(BuildContext context, String _title, String _hint, IconData _icon) {
     String inputUser;
 
-    return new Padding(
-        padding: EdgeInsets.only(top: 20),
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-
-       // Text(
-       //   _title,
-       //   style: TextStyle(fontSize: 15),
-       // ),
-
-        Expanded(
-          child: TextField(
+    return new ListTile(
+      leading: Icon(_icon, size: 35),
+          title: TextField(
           decoration: InputDecoration(
             labelText: _title,
-            labelStyle: TextStyle(fontSize: 16, color: Colors.black45),
+            //labelStyle: TextStyle(fontSize: 16, color: Colors.black45),
             hintText: _hint,
-            hintStyle: TextStyle(fontSize: 10, color: Colors.black12),
+            //hintStyle: TextStyle(fontSize: 13, color: Colors.black12),
           ),
 //                        validator: (input) => input.length < 0
 //                            ? "You have to write something!"
 //                            : null,
           keyboardType: TextInputType.text,
           onChanged: (input) => inputUser = input,
-        ),)
-      ],
-    ));
-  }
-}
+        ));
+    }}
