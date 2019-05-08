f5cb05b6e117def30bdc1f9b2d7a8ead8abd2ecd
Felix Strobel <mail@felixstrobel.de>
2019-05-08 14:30:37 +0200

    temp

--- lust/lib/widgets/capacityPage/capacityGraph.dart
+++ lust/lib/widgets/capacityPage/capacityGraph.dart
@@ -19,33 +19,52 @@ class CapacityGraph extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new charts.BarChart(
-      seriesList,
-
-      animate: animate,
-    );
+    return new charts.OrdinalComboChart(seriesList,
+        animate: animate,
+        // Configure the default renderer as a bar renderer.
+        defaultRenderer: new charts.BarRendererConfig(
+            groupingType: charts.BarGroupingType.grouped, strokeWidthPx: 2.0),
+        // Custom renderer configuration for the line series. This will be used for
+        // any series that does not define a rendererIdKey.
+        customSeriesRenderers: [
+          new charts.LineRendererConfig(
+            // ID used to link series to this renderer.
+              customRendererId: 'customLine')
+        ]);
   }
 
   static List<charts.Series<PercentPerHour, String>> _createChartData() {
-    final data = [
-      new PercentPerHour(10, "8"),
-      new PercentPerHour(30, "10"),
-      new PercentPerHour(50, "12"),
-      new PercentPerHour(70, "14"),
-      new PercentPerHour(90, "16"),
-      new PercentPerHour(90, "18"),
-      new PercentPerHour(70, "20"),
-      new PercentPerHour(50, "22"),
+    final dataA = [
+      new PercentPerHour(17, "8"),
+      new PercentPerHour(35, "10"),
+      new PercentPerHour(13, "12"),
+      new PercentPerHour(7, "14"),
+      new PercentPerHour(22, "16"),
+      new PercentPerHour(42, "18"),
+      new PercentPerHour(87, "20"),
+      new PercentPerHour(69, "22"),
       ];
 
+    var hour = CapacityTime.getTimeForCapacityGraph();
+    print('hour in capacity ' + hour.toString());
     return [
       new charts.Series<PercentPerHour, String>(
-        id: 'Sales',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        id: 'Other',
+        colorFn: (PercentPerHour percent, __) => percent.hour == hour.toString()
+              ? charts.MaterialPalette.red.shadeDefault
+            : charts.MaterialPalette.blue.shadeDefault,
         domainFn: (PercentPerHour percent, _) => percent.hour,
         measureFn: (PercentPerHour percent, _) => percent.percent,
-        data: data,
-      )
+        data: dataA,
+      ),
+//      new charts.Series<PercentPerHour, String>(
+//        id: 'Current',
+//        colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
+//        domainFn: (PercentPerHour percent, _) => percent.hour,
+//        measureFn: (PercentPerHour percent, _) => percent.percent,
+//        data: dataB,
+//      )..setAttribute(charts.rendererIdKey, 'customLine'),
+
     ];
   }
 }
