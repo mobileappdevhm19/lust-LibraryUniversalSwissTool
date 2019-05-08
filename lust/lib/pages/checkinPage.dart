9eab7f0c1eadf2eed3801e533997c0749a34d7d1
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-05-08 23:50:28 +0200

    recreating brunch

---
+++ lust/lib/pages/checkinPage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lust/widgets/checkinPage/buttonCheck.dart';
+
+class CheckInOut extends StatefulWidget {
+  @override
+  _CheckInOutState createState() => _CheckInOutState();
+}
+
+class _CheckInOutState extends State<CheckInOut> {
+  final String _title = "Check In/Out";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(_title)),
+        body: Center(
+            child: Padding(
+                padding: EdgeInsets.only(right: 10, left: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    TextBox.getTextBox(context, "Subject",
+                        "Good luck with that!", Icons.local_library),
+                    TextBox.getTextBox(
+                        context, "Time goal", "Be realistic!", Icons.timer),
+                    TextBox.getTextBox(context, "Locker number",
+                        "I know that is difficult to remember", Icons.lock),
+                    ButtonCheck(),
+                    ]))));
+  }
+
+}
