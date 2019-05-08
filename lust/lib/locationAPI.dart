01e41172a97196885c278232a7cb295fda9554c8
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-05-08 11:38:32 +0200

    created class LIBS + used in the API

--- lust/lib/locationAPI.dart
+++ lust/lib/locationAPI.dart
@@ -1,12 +1,13 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:lust/models/bibliotheken/bibs.dart';
 
 class LocationAPI{
   static Future getLocation() async {
     //LIB: {48.153954, 11.552292}
-
+    Library libHM = new Library(48.153954, 11.552292);
     Position positionStudent = await Geolocator().getLastKnownPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
     List<Placemark> positionLib = await Geolocator().placemarkFromAddress("Lothstraße 13D, 80335 München");
 
-    double distance = await Geolocator().distanceBetween(positionStudent.latitude, positionStudent.longitude, positionLib.latitude, positionLib.longitude)
+    double distance = await Geolocator().distanceBetween(positionStudent.latitude, positionStudent.longitude, libHM.latitude, libHM.latitude);
   }
 }
