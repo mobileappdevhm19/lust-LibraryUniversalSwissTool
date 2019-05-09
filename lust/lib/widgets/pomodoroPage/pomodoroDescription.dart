e0bb97426a12dbaf5a1bc4068a0d55a9b3e29ece
mike <lauterba@hm.edu>
2019-05-10 01:21:07 +0200

    setting times are now not so harcoded as before
    values set in  pages/PomodoroPage
         via Konstruktor etc. this class calls subclasses with same values

--- lust/lib/widgets/pomodoroPage/pomodoroDescription.dart
+++ lust/lib/widgets/pomodoroPage/pomodoroDescription.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 class pomodoroDescription extends StatelessWidget {
+  double periodTime;
+  double shortBreakTime;
+  double longBreakTime;
+  int countPeriods;
+
+  pomodoroDescription(double periodTime, double shortBreakTime, double longBreakTime, int countPeriods){
+    this.periodTime=periodTime;
+    this.shortBreakTime=shortBreakTime;
+    this.longBreakTime=longBreakTime;
+    this.countPeriods=countPeriods;
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -16,8 +28,8 @@ class pomodoroDescription extends StatelessWidget {
                   style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
                   textAlign: TextAlign.center,
                 )),
-                const Expanded(child: Text(
-                  "25.5 min",
+                Expanded(child: Text(
+                  "$periodTime min",
                   textScaleFactor: 0.8,
                   style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
                   textAlign: TextAlign.center,
@@ -37,8 +49,8 @@ class pomodoroDescription extends StatelessWidget {
                   style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
                   textAlign: TextAlign.center,
                 )),
-                const Expanded(child: Text(
-                  "5 / 15 min",
+                Expanded(child: Text(
+                  "$shortBreakTime min"+" / "+"$longBreakTime min",
                   textScaleFactor: 0.8,
                   style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
                   textAlign: TextAlign.center,
@@ -58,14 +70,24 @@ class pomodoroDescription extends StatelessWidget {
                   style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
                   textAlign: TextAlign.center,
                 )),
-                const Expanded(child: Text(
-                  "4",
+                Expanded(child: Text(
+                  '$countPeriods',
                   textScaleFactor: 0.8,
                   style: TextStyle(fontSize: 20.0, letterSpacing: 2.0),
                   textAlign: TextAlign.center,
                 )),
               ],
             ),
+            new SizedBox(
+              height: 10.0,
+              child: new Center(
+                child: new Container(
+                  margin: new EdgeInsetsDirectional.only(start: 1.0, end: 1.0),
+                  height: 5.0,
+                  color: Colors.red,
+                ),
+              ),
+            )
           ]),
     );
 
