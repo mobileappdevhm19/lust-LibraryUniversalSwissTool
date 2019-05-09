c38c8dd8afbd9fe28258327ba04af8eb066df995
andre <brauni.andre@gmail.com>
2019-05-09 16:29:37 +0200

    change capacitypage to extend from statefulpage

--- lust/lib/pages/capacityPage.dart
+++ lust/lib/pages/capacityPage.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+
+import 'package:lust/pages/statefulPage.dart';
+
 import 'package:lust/widgets/capacityPage/capacityGraph.dart';
 import 'package:lust/widgets/capacityPage/capacityInfo.dart';
 
-class CapacityPage extends StatefulWidget {
+import 'package:lust/widgets/utils/menuDrawer.dart';
+
+class CapacityPage extends StatefulPage {
+
+  CapacityPage({this.title = "Capacity", this.icon = Icons.equalizer});
+
+  final String title;
+  final icon;
+
   @override
-  _CapacityPageState createState() => new _CapacityPageState();
+  _CapacityPageState createState() => new _CapacityPageState(title, icon);
 }
 
 class _CapacityPageState extends State<CapacityPage> {
-  final _title = "Capacity";
+
+  _CapacityPageState(this.title, this.icon);
+
+  final String title;
+  final icon;
 
   @override
   Widget build(BuildContext context) {
@@ -17,8 +32,9 @@ class _CapacityPageState extends State<CapacityPage> {
 
     return Scaffold(
         appBar: AppBar(
-          title: Text(_title),
+          title: Text(title),
         ),
+        drawer: MenuDrawer.getDrawer(context),
         body: Center(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
