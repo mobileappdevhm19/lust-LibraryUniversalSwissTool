26235a9fe92ca690c8f3bcfc35480d0c96b2a1ee
Felix Strobel <mail@felixstrobel.de>
2019-05-11 10:15:38 +0200

    Add an util for widgets that handles the time.
    * makeHourEven for the capacity graph. Because we need the last even hour.

--- lust/lib/widgets/utils/timeHandler.dart
+++ lust/lib/widgets/utils/timeHandler.dart
@@ -3,10 +3,10 @@
 
 class TimeHandler {
   /// Getting the last even hour. E.g. for 7:42 it will return 6.
-  static int makeHourEven(int hour) {
-    if (hour < 0 || hour > 24) {
-      throw ArgumentError();
-    }
+  static int makeHourEven(num hour) {
+    if (hour < 0 || hour > 24)
+      throw new ArgumentError(
+          "The input is below 0 or above 24. That's not possible");
     return hour % 2 == 0 ? hour : hour - 1;
   }
 }
