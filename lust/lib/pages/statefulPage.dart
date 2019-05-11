dd723c81f3e1bd41fc2893d6875f33ce2a4acc11
andre <brauni.andre@gmail.com>
2019-05-11 18:28:05 +0200

    change order of constructor

--- lust/lib/pages/statefulPage.dart
+++ lust/lib/pages/statefulPage.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 abstract class StatefulPage extends StatefulWidget {
 
-  StatefulPage({this.title, this.icon});
-
   final String title;
   final icon;
+
+  StatefulPage(this.title, this.icon);
 }
