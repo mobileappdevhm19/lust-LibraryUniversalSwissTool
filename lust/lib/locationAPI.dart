d7d064b7c8960ae84ffb1fcf4deb1ee171cd56d1
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-05-13 20:06:51 +0200

    I dont know in which folder should be this

--- lust/lib/locationAPI.dart
+++ lust/lib/locationAPI.dart
@@ -1,13 +1,20 @@
 import 'package:geolocator/geolocator.dart';
-import 'package:lust/models/bibliotheken/bibs.dart';
+import 'package:lust/models/bibs.dart';
 
 class LocationAPI{
   static Future getLocation() async {
-    //LIB: {48.153954, 11.552292}
+    //LIB: {48.153954, 11.552292}; PENDING: this lib should be on firebase
     Library libHM = new Library(48.153954, 11.552292);
     Position positionStudent = await Geolocator().getLastKnownPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
     List<Placemark> positionLib = await Geolocator().placemarkFromAddress("Lothstraße 13D, 80335 München");
 
     double distance = await Geolocator().distanceBetween(positionStudent.latitude, positionStudent.longitude, libHM.latitude, libHM.latitude);
+
+    if (distance <100){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
