cd708f72b615e9f90c3daf1f9acb2e989c078924
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-05-22 16:49:38 +0200

    rebasing

---
+++ lust/lib/utils/locationAPI.dart
@@ -0,0 +1,21 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:lust/models/bibs.dart';
+
+class LocationAPI{
+  static Future getLocation() async {
+    //LIB: {48.153954, 11.552292}
+    Library libHM = new Library(48.153954, 11.552292);
+    Position positionStudent = await Geolocator().getLastKnownPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
+    List<Placemark> positionLib = await Geolocator().placemarkFromAddress("Lothstraße 13D, 80335 München");
+
+    double distance = await Geolocator().distanceBetween(positionStudent.latitude, positionStudent.longitude, libHM.latitude, libHM.latitude);
+
+    if (distance <100){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
+
