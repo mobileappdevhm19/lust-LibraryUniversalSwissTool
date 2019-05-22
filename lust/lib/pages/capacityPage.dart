c810d5a18748788bdb4556c928421215309a84e3
Felix Strobel <mail@felixstrobel.de>
2019-05-22 20:18:53 +0200

    Improve visualization

--- lust/lib/pages/capacityPage.dart
+++ lust/lib/pages/capacityPage.dart
@@ -7,7 +7,6 @@ import 'package:lust/models/library.dart';
 import 'package:lust/widgets/utils/menuDrawer.dart';
 
 class CapacityPage extends StatefulWidget {
-
   static String title = "Capacity";
   static IconData icon = Icons.equalizer;
 
@@ -16,39 +15,34 @@ class CapacityPage extends StatefulWidget {
 }
 
 class _CapacityPageState extends State<CapacityPage> {
-
   final String title;
   final icon;
 
   _CapacityPageState(this.title, this.icon);
-  final _title = "Capacity";
+
   final Library library = new Library.withSampleData();
 
   @override
   Widget build(BuildContext context) {
-
     var _height = MediaQuery.of(context).size.height;
-
+    _height -= 85;
     return Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
         drawer: MenuDrawer.getDrawer(context),
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  height: _height*0.60,
-                  child: CapacityGraph.fromLibrary(library),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.all(10.0),
-                  child: CapacityInfo(library),
-                )
-              ]),
-        ));
+        body: ListView(children: [
+          Container(
+            margin: const EdgeInsets.only(left: 10),
+            height: _height * 0.66,
+            child: CapacityGraph.fromLibrary(library),
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: _height * 0.33,
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            child: CapacityInfo(library),
+          )
+        ]));
   } // build
-
 }
