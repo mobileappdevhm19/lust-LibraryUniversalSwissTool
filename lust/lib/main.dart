a69350e71f316b77d587a1f6aae0d1a8b9de1f32
mike <lauterba@hm.edu>
2019-05-22 13:32:46 +0200

    first wirkoing notification try

--- lust/lib/main.dart
+++ lust/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:lust/pages/capacityPage.dart';
 import 'package:lust/pages/checkinPage.dart';
 
-void main() => runApp(Lust());
+void main() async=> runApp(Lust());
 
 class Lust extends StatelessWidget {
   // This widget is the root of your application.
