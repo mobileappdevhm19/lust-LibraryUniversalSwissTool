ddf6d7aa74f6976873ac94754598d3d4d2f915c8
mike <lauterba@hm.edu>
2019-05-22 13:32:46 +0200

    first wirkoing notification try

--- lust/lib/widgets/pomodoroPage/pomodoroTimer.dart
+++ lust/lib/widgets/pomodoroPage/pomodoroTimer.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'dart:ui';
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+//import 'package:lust/widgets/utils/notifications.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+
+
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
 
 enum Status{
@@ -19,7 +26,8 @@ class statusClass {
 class PomodoroTimer extends StatefulWidget {
   PomodoroTimerState ptS;
   PomodoroTimer(int periodTime, int shortBreakTime, int longBreakTime, int countPeriods) {
-      ptS=new PomodoroTimerState(periodTime, shortBreakTime, longBreakTime, countPeriods);
+    //flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    ptS=new PomodoroTimerState(periodTime, shortBreakTime, longBreakTime, countPeriods);
   }
 
   @override
@@ -53,8 +61,23 @@ class PomodoroTimerState extends State<PomodoroTimer> {
 
     actStatusText=initialStatusText;
     setActTimeMinutesSeconds(); //so there stand 00:00 when start this page
-    //notification=notifications();
   }
+
+  @override
+  initState() {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    super.initState();
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    var initializationSettingsAndroid =
+    AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +92,9 @@ class PomodoroTimerState extends State<PomodoroTimer> {
                 child:
                   RaisedButton(
                     color: startStopBtnColor,
-                    onPressed: () {startStopButtonClicked();},
+                    onPressed: () async {
+                      await _showNotification();
+                    },
                     child: Text(
                         '$startStopBtnText',
                         style: TextStyle(fontSize: 40)
@@ -132,7 +157,7 @@ class PomodoroTimerState extends State<PomodoroTimer> {
   void start(){
     startStopBtnText="Stop";
     startStopBtnColor=Colors.red;
-    if(actTimerSeconds==0){ //to initialize the status and time
+    if(actTimerSeconds==0){
       changeStatus();
     }
     else{
@@ -232,11 +257,15 @@ class PomodoroTimerState extends State<PomodoroTimer> {
             changeStatus();
           } else {
             setActTimeMinutesSeconds();
+            showNormalNoti();
           }
         }),
     );
   }
 
+  Future<void> showNormalNoti() async{
+    await _showNotification();
+  }
 
   void setActTimeMinutesSeconds(){
     int minutes=(actTimerSeconds/60).toInt();
@@ -259,5 +288,88 @@ class PomodoroTimerState extends State<PomodoroTimer> {
     ret +="\n";
     ret +=(countPeriods-actPeriod).toString() +"periods till next long break";
     return ret;
+  }
+
+  Future<void> _showNotification() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'plain title', 'plain body', platformChannelSpecifics,
+        payload: 'item x');
+  }
+
+  Future<void> onSelectNotification(String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
+    }
+
+    await Navigator.push<void>(
+      context,
+      MaterialPageRoute(builder: (context) => SecondScreen(payload)),
+    );
+  }
+
+  Future<void> onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    // display a dialog with the notification details, tap ok to go to another page
+    await showDialog<void>(
+      context: context,
+      builder: (BuildContext context) => CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(body),
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: Text('Ok'),
+            onPressed: () async {
+              Navigator.of(context, rootNavigator: true).pop();
+              await Navigator.push<void>(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SecondScreen(payload),
+                ),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  final String payload;
+  SecondScreen(this.payload);
+  @override
+  State<StatefulWidget> createState() => SecondScreenState();
+}
+
+class SecondScreenState extends State<SecondScreen> {
+  String _payload;
+  @override
+  void initState() {
+    super.initState();
+    _payload = widget.payload;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen with payload: " + _payload),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
   }
 }
