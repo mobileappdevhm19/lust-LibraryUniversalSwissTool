7fc494304d818440dd84f7e5cfda460bc5de5c32
Felix Strobel <mail@felixstrobel.de>
2019-05-22 20:16:10 +0200

    Better oneLineText

--- lust/lib/widgets/utils/oneLineText.dart
+++ lust/lib/widgets/utils/oneLineText.dart
@@ -5,18 +5,24 @@ class OneLineText extends StatelessWidget {
   final double fontSize;
   final double letterSpacing;
   final double textScaleFactor;
+  final bool softWrap;
+  final TextAlign textAlign;
 
   OneLineText({
     this.text = "Default Text",
     this.fontSize = 20.0,
     this.letterSpacing = 2.0,
     this.textScaleFactor = 0.8,
+    this.softWrap = true,
+    this.textAlign = TextAlign.center,
   });
 
   @override
   Widget build(BuildContext context) {
     return new Text(
       text,
+      softWrap: softWrap,
+      textAlign: textAlign,
       style: new TextStyle(fontSize: fontSize, letterSpacing: letterSpacing),
       textScaleFactor: textScaleFactor,
     );
