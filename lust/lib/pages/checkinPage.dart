500288b7299e7f8ebdbc472b141c0345a0846293
andre <brauni.andre@gmail.com>
2019-05-22 17:47:06 +0200

    change checkinpage internally to new menudrawer

--- lust/lib/pages/checkinPage.dart
+++ lust/lib/pages/checkinPage.dart
@@ -3,18 +3,29 @@ import 'package:flutter/widgets.dart';
 import 'package:lust/widgets/checkinPage/buttonCheck.dart';
 import 'package:lust/widgets/utils/getTextBox.dart';
 
+import 'package:lust/widgets/utils/menuDrawer.dart';
+
 class CheckinPage extends StatefulWidget {
+
+  static String title = "Check In/Out";
+  static IconData icon = Icons.check_circle_outline;
+
   @override
-  _CheckinPageState createState() => _CheckinPageState();
+  _CheckinPageState createState() => _CheckinPageState(title, icon);
 }
 
 class _CheckinPageState extends State<CheckinPage> {
-  final String _title = "Check In/Out";
+
+  final String title;
+  final icon;
+
+  _CheckinPageState(this.title, this.icon);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(_title)),
+        appBar: AppBar(title: Text(title)),
+        drawer: MenuDrawer.getDrawer(context),
         body: Center(
             child: Padding(
                 padding: EdgeInsets.only(right: 10, left: 10),
