baf8a2f41455f1b0a50645a22644998713066ae2
Felix Strobel <mail@felixstrobel.de>
2019-05-23 13:27:36 +0200

    use listView for checkin

--- lust/lib/pages/checkinPage.dart
+++ lust/lib/pages/checkinPage.dart
@@ -6,7 +6,6 @@ import 'package:lust/widgets/utils/getTextBox.dart';
 import 'package:lust/widgets/utils/menuDrawer.dart';
 
 class CheckinPage extends StatefulWidget {
-
   static String title = "Check In/Out";
   static IconData icon = Icons.check_circle_outline;
 
@@ -15,7 +14,6 @@ class CheckinPage extends StatefulWidget {
 }
 
 class _CheckinPageState extends State<CheckinPage> {
-
   final String title;
   final icon;
 
@@ -23,23 +21,42 @@ class _CheckinPageState extends State<CheckinPage> {
 
   @override
   Widget build(BuildContext context) {
+    var _height = MediaQuery.of(context).size.height;
+    _height -= 85;
+
     return Scaffold(
         appBar: AppBar(title: Text(title)),
         drawer: MenuDrawer.getDrawer(context),
-        body: Center(
-            child: Padding(
-                padding: EdgeInsets.only(right: 10, left: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    TextBox.getTextBox(context, "Subject",
-                        "Good luck with that!", Icons.local_library),
-                    TextBox.getTextBox(
-                        context, "Time goal", "Be realistic!", Icons.timer),
-                    TextBox.getTextBox(context, "Locker number",
-                        "I know that is difficult to remember", Icons.lock),
-                    ButtonCheck(),
-                    ]))));
-  }
+        body: ListView(children: [
+          Container(
+            margin: const EdgeInsets.only(left: 10),
+            height: _height * 0.4,
+            child: Column(
+              children: <Widget>[
+                TextBox.getTextBox(context, "Subject", "Good luck with that!",
+                    Icons.local_library),
+                TextBox.getTextBox(
+                    context, "Time goal", "Be realistic!", Icons.timer),
+                TextBox.getTextBox(context, "Locker number",
+                    "I know that is difficult to remember", Icons.lock),
+              ],
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            height: _height * 0.6,
+//              margin: const EdgeInsets.only(left: 10, right: 10),
+            child: ButtonCheck(),
+          ),
 
+//    TextBox.getTextBox(context, "Subject",
+//    "Good luck with that!", Icons.local_library),
+//    TextBox.getTextBox(
+//    context, "Time goal", "Be realistic!", Icons.timer),
+//    TextBox.getTextBox(context, "Locker number",
+//    "I know that is difficult to remember", Icons.lock),
+//    ButtonCheck(),
+//                    ]))
+        ]));
+  }
 }
