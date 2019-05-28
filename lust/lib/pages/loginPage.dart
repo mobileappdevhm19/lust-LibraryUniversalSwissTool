165026bd0083c1b9fae4c25dc0f5a86dbe3b8917
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-05-28 20:22:14 +0200

    loginPage layout way better

--- lust/lib/pages/loginPage.dart
+++ lust/lib/pages/loginPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lust/widgets/loginPage/appLogo.dart';
 import 'package:lust/widgets/loginPage/buttonLogIn.dart';
 import 'package:lust/widgets/utils/getTextBox.dart';
 
@@ -15,24 +16,27 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text(_title)),
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+        body: Form(
+            //key: _globalKey,
+            child: ListView(
           children: <Widget>[
             //PENDING: add a circular progress/similar indicator
             //TOP of the SCREEN: app icon!!
-
+            appLogo(),
             TextBox.getTextBox(context, "E-mail",
                 "Do you already have an account?", Icons.email),
             TextBox.getTextBox(context, "Password", "123456", Icons.security),
+            //SizedBox(height: 20, width: double.infinity),
             ButtonLogin(
-                buttonText: "Log in",
-                whichButton: true,
-                buttonColor: Colors.blue),
+              buttonText: "Log in",
+              whichButton: true,
+              buttonColor: Colors.blue,
+            ),
             ButtonLogin(
-                buttonText: "Register",
-                whichButton: false,
-                buttonColor: Colors.red)
+              buttonText: "Register",
+              whichButton: false,
+              buttonColor: Colors.red,
+            )
           ],
         )));
   }
---
+++ lust/lib/widgets/loginPage/appLogo.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Widget appLogo() {
+  return Padding(
+    padding: EdgeInsets.only(top: 20),
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+    child: Image(image: AssetImage('assets/popper.png')),
+    radius: 80,
+  ));
+}
