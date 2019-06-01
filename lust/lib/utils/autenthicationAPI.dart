780a9518d2c85952a174378fb752a9c4bd5daeef
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-06-01 11:28:49 +0200

    arranged auth API

--- lust/lib/utils/autenthicationAPI.dart
+++ lust/lib/utils/autenthicationAPI.dart
@@ -2,22 +2,27 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-abstract class AutenthicationAPI {
-  Future<String> signIn(String email, String password);
-
-  Future<String> signUp(String email, String password);
-
-  Future<FirebaseUser> getCurrentUser();
-
-  Future<void> sendEmailVerification();
-
-  Future<void> signOut();
-
-  Future<bool> isEmailVerified();
-}
-//PENDING: check it without the 'abstract'
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Future<String> signUp(String email, String password) async {
+    FirebaseUser user = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return user.uid;
+  }
+
+  @override
+  Future<String> signIn(String email, String password) async {
+    FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);g
+    return user.uid;
+  }
+
+  @override
+  Future<void> signOut() async {
+    return _firebaseAuth.signOut();
+  }
 
   @override
   Future<FirebaseUser> getCurrentUser() async {
@@ -35,42 +40,5 @@ class Auth {
   Future<void> sendEmailVerification() async {
     FirebaseUser user = await _firebaseAuth.currentUser();
     user.sendEmailVerification();
-  }
-
-  @override
-  Future<String> signIn(String email, String password) async {
-    FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(
-        email: email, password: password);
-    return user.uid;
-  }
-
-  @override
-  Future<void> signOut() async {
-    return _firebaseAuth.signOut();
-  }
-
-  @override
-  Future<String> signUp(String email, String password) async {
-    FirebaseUser user = await _firebaseAuth.createUserWithEmailAndPassword(
-        email: email, password: password);
-    return user.uid;
-  }
-}
-
-class checkUser {
-  //this formKey is absolutely essential to link the 'signIn/up' button w/the textBox
-  //the FormState class contains the validate method. When the validate method is called, it will run the validator function for each text field in the form.
-  final _formKey = new GlobalKey<FormState>();
-
-  bool _validateKey(){
-    final form = _formKey.currentState;
-    if(form.validate()){
-      form.save();
-      return true;
-    }
-    return false;
-  }
-  Future _checkUser() async{
-
   }
 }
