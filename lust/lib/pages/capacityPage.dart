1d455687700dad5d6983f0e6c23b63c18350576e
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-06-09 23:12:57 +0200

    sign out functionality

--- lust/lib/pages/capacityPage.dart
+++ lust/lib/pages/capacityPage.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
-
+import 'package:lust/utils/autenthicationAPI.dart';
+import 'package:lust/models/library.dart';
 import 'package:lust/widgets/capacityPage/capacityGraph.dart';
 import 'package:lust/widgets/capacityPage/capacityInfo.dart';
-import 'package:lust/models/library.dart';
-
 import 'package:lust/widgets/utils/menuDrawer.dart';
+import 'package:lust/widgets/utils/getAppBar.dart';
+
 
 class CapacityPage extends StatefulWidget {
+  CapacityPage({this.auth});
+
   static String title = "Capacity";
   static IconData icon = Icons.equalizer;
+  BaseAuth auth;
 
   @override
   _CapacityPageState createState() => new _CapacityPageState(title, icon);
 }
 
 class _CapacityPageState extends State<CapacityPage> {
-  final String title;
+  final String _title;
   final icon;
 
-  _CapacityPageState(this.title, this.icon);
+  _CapacityPageState(this._title, this.icon);
 
   final Library library = new Library.withSampleData();
 
@@ -27,13 +31,12 @@ class _CapacityPageState extends State<CapacityPage> {
     var _height = MediaQuery.of(context).size.height;
     _height -= 85;
     return Scaffold(
-        appBar: AppBar(
+        /*appBar: AppBar(
           title: Text(title),
-        ),
+        ),*/
+        appBar: PreferredSize(child: GetAppBar(_title), preferredSize: Size.fromHeight(50)),
         drawer: MenuDrawer.getDrawer(context),
-        body: ListView(
-            reverse: false,
-            children: [
+        body: ListView(reverse: false, children: [
           Container(
             margin: const EdgeInsets.only(left: 10),
             height: _height * 0.60,
@@ -47,4 +50,13 @@ class _CapacityPageState extends State<CapacityPage> {
           ),
         ]));
   } // build
+
+  _signOut() {
+    try {
+      widget.auth.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+
 }
