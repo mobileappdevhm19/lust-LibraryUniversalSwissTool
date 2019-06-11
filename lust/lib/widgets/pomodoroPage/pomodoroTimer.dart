19705c373aa807cb55c060473bd9d1b5e2ed14cd
mike <lauterba@hm.edu>
2019-06-11 20:29:44 +0200

    first try, that timer also runs in background

--- lust/lib/widgets/pomodoroPage/pomodoroTimer.dart
+++ lust/lib/widgets/pomodoroPage/pomodoroTimer.dart
@@ -6,6 +6,8 @@ import 'dart:typed_data';
 import 'package:flutter/services.dart';
 //import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:lust/pages/pomodoroPage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 
 //FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
@@ -13,6 +15,10 @@ import 'package:lust/pages/pomodoroPage.dart';
 enum Status{
   nothing, learning, shortBreak, longBreak
 }
+
+const StartTime_KEY = "startTime";
+const IsRunning_KEY = "isRunning";
+const StopTime_KEY = "stopTime";
 
 //like struct in c++
 class statusClass {
@@ -37,11 +43,14 @@ class PomodoroTimerState extends State<PomodoroTimer> {
   int countPeriods;
 
   int actPeriod=0;
-  int actTimerSeconds=0;
+  int actTimerSeconds;
   String actTimeMinutesSeconds="";
   Status actStatus=Status.nothing; //initial
   String initialStatusText="Click on 'Start' to start the pomodoro timer";
   String actStatusText;
+  int startTime;
+  bool isRunning=false;
+  int stopTime; //sec, since 1970, set, when stopButton clicked
 
   Timer _timer;
 
@@ -49,6 +58,80 @@ class PomodoroTimerState extends State<PomodoroTimer> {
   String resetBtnText="reset";
   ColorSwatch startStopBtnColor=Colors.green;
   //notifications notification;
+
+  @override
+  void initState() {
+    super.initState();
+
+    actTimerSeconds=0;
+    setActTimeMinutesSeconds(); //for 00:00 at first
+
+
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    setActTimeMinutesSeconds(); //for 00:00 at first
+    // Load persisted fetch events from SharedPreferences
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    int actTime = new DateTime.now().millisecondsSinceEpoch;
+    actTime = (actTime / 1000).toInt();
+    startTime = prefs.getInt(StartTime_KEY);
+    /*setState(() {
+      actErrors += "startTime $startTime \n";
+    });*/
+
+    int stopT = prefs.getInt(StopTime_KEY);
+    if (stopT != null) {
+      stopTime = stopT;
+    } else {
+      stopTime = actTime;
+    }
+    /*setState(() {
+      actErrors += "stop $stopTime \n";
+    });*/
+
+    if (startTime != null) {
+      actTimerSeconds = actTime - startTime;
+      setState(() {
+        setActTimeMinutesSeconds();
+      });
+
+      isRunning = prefs.getBool(IsRunning_KEY);
+      if(isRunning){
+        if (_timer != null) {
+          _timer.cancel(); //stop timer if exist
+        }
+        startStopBtnText = "Stop";
+        startStopBtnColor = Colors.red;
+        startTimer(); //start Timer with actual values
+      }
+      else{
+        actTimerSeconds = actTimerSeconds-(actTime-stopTime); //subtrac the time beetween last stop click and now
+      }
+    } else {
+      startTime = actTime;
+      actTimerSeconds = 0;
+      isRunning=false; //when no seconds count, the timer cannot be started
+    }
+
+    setState(() {
+      setActTimeMinutesSeconds();
+    });
+
+
+
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+  }
+
+
+
+
 
   PomodoroTimerState(int periodTime, int shortBreakTime, int longBreakTime, int countPeriods) {
     statuslist.add(statusClass(-1, ""));
@@ -144,22 +227,44 @@ class PomodoroTimerState extends State<PomodoroTimer> {
       }
     });
   }
-  void stop(){
-    startStopBtnText="Start";
-    startStopBtnColor=Colors.green;
-    if(_timer !=null){
-      _timer.cancel();
+  void stop() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    //if(isRunning){
+    setState(() {
+      startStopBtnText = "Start";
+      startStopBtnColor = Colors.green;
+    });
+    if (_timer != null) {
+      _timer.cancel(); //stop timer if exist
     }
+    // }
+    isRunning=false;
+
+
+
+    //for background
+
+    prefs.setBool(IsRunning_KEY, isRunning);
+    int actTime = new DateTime.now().millisecondsSinceEpoch;
+    actTime = (actTime / 1000).toInt();
+    stopTime=actTime;
+    startTime=stopTime-actTimerSeconds;
+    /*setState(() {
+      actErrors += "startTime $startTime \n";
+    });*/
+    prefs.setInt(StopTime_KEY, stopTime);
   }
-  void start(){
-    startStopBtnText="Stop";
-    startStopBtnColor=Colors.red;
-    if(actTimerSeconds==0){
-      changeStatus();
-    }
-    else{
-      startTimer();
-    }
+
+
+
+  void start() async {
+    setState(() {
+      startStopBtnText = "Stop";
+      startStopBtnColor = Colors.red;
+    });
+    startTimer(); //start a new timer
+    isRunning=true;
   }
 
 
@@ -238,7 +343,16 @@ class PomodoroTimerState extends State<PomodoroTimer> {
   }
 
 
-  void startTimer(){
+  void startTimer() async{
+    //for background
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int actTime = new DateTime.now().millisecondsSinceEpoch;
+    actTime = (actTime / 1000).toInt();
+    startTime=actTime-actTimerSeconds;
+
+    prefs.setInt(StartTime_KEY, startTime);
+    prefs.setBool(IsRunning_KEY, true);
     const oneSec = const Duration(seconds: 1);
     _timer = new Timer.periodic(
         oneSec,
@@ -255,22 +369,28 @@ class PomodoroTimerState extends State<PomodoroTimer> {
     );
   }
 
+
   /*Future<void> showNormalNoti() async{
     await _showNotification();
   }*/
-
-  void setActTimeMinutesSeconds(){
-    int minutes=(actTimerSeconds/60).toInt();
-    String minutesStirng=setFirst0(minutes);
-    String secondsString=setFirst0(actTimerSeconds-(minutes*60));
-
-    actTimeMinutesSeconds="$minutesStirng : $secondsString";
-  }
-  String setFirst0(int number){
-    if(number<10){
-      return "0$number";
+  ///only for textual output
+  void setActTimeMinutesSeconds() {
+    if (actTimerSeconds == null) {
+      actTimerSeconds = 0;
     }
-    else{
+
+    int minutes = (actTimerSeconds / 60).toInt();
+    String minutesString = setFirst0(minutes);
+    String secondsString = setFirst0(actTimerSeconds - (minutes * 60));
+
+    actTimeMinutesSeconds = "$minutesString : $secondsString";
+    //return actTimeMinutesSeconds;
+  }
+
+  String setFirst0(int number) {
+    if (number < 10) {
+      return "0$number";
+    } else {
       return number.toString();
     }
   }
