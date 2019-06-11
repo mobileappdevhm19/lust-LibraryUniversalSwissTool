44dd4d3db04214735360c6095b955fa654085eee
Mateo Gonzalez <hm-gonzale1@hm.edu>
2019-06-11 16:15:16 +0200

    auth implementation

--- lust/lib/pages/capacityPage.dart
+++ lust/lib/pages/capacityPage.dart
@@ -6,23 +6,22 @@ import 'package:lust/widgets/capacityPage/capacityInfo.dart';
 import 'package:lust/widgets/utils/menuDrawer.dart';
 import 'package:lust/widgets/utils/getAppBar.dart';
 
-
 class CapacityPage extends StatefulWidget {
   CapacityPage({this.auth});
 
   static String title = "Capacity";
   static IconData icon = Icons.equalizer;
-  BaseAuth auth;
+  final BaseAuth auth;
 
   @override
   _CapacityPageState createState() => new _CapacityPageState(title, icon);
 }
 
 class _CapacityPageState extends State<CapacityPage> {
-  final String _title;
+  final String title;
   final icon;
 
-  _CapacityPageState(this._title, this.icon);
+  _CapacityPageState(this.title, this.icon);
 
   final Library library = new Library.withSampleData();
 
@@ -31,32 +30,24 @@ class _CapacityPageState extends State<CapacityPage> {
     var _height = MediaQuery.of(context).size.height;
     _height -= 85;
     return Scaffold(
-        /*appBar: AppBar(
+        appBar: AppBar(
           title: Text(title),
-        ),*/
-        appBar: PreferredSize(child: GetAppBar(_title), preferredSize: Size.fromHeight(50)),
+        ),
         drawer: MenuDrawer.getDrawer(context),
-        body: ListView(reverse: false, children: [
-          Container(
-            margin: const EdgeInsets.only(left: 10),
-            height: _height * 0.60,
-            child: CapacityGraph.fromLibrary(library),
-          ),
-          Container(
-            alignment: Alignment.center,
-            height: _height * 0.4,
-            margin: const EdgeInsets.only(left: 10, right: 10),
-            child: CapacityInfo(library),
-          ),
-        ]));
+        body: ListView(
+            reverse: false,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                height: _height * 0.60,
+                child: CapacityGraph.fromLibrary(library),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: _height * 0.4,
+                margin: const EdgeInsets.only(left: 10, right: 10),
+                child: CapacityInfo(library),
+              ),
+            ]));
   } // build
-
-  _signOut() {
-    try {
-      widget.auth.signOut();
-    } catch (e) {
-      print(e);
-    }
-  }
-
 }
