ae5f6362faead311a41bb7aedeaec6d5158be7a2
mike <lauterba@hm.edu>
2019-06-28 10:57:15 +0200

    tests pass

--- lust/lib/pages/pomodoroPage/pomodoroPage.dart
+++ lust/lib/pages/pomodoroPage/pomodoroPage.dart
@@ -18,21 +18,30 @@ class _PomodoroState extends State<PomodoroPage> {
   final icon;
 
   //all in minutes
-  int periodTime=10;
-  int shortBreakTime=4;
-  int longBreakTime=3;
-  int countPeriods=9;
+  int periodTime;
+  int shortBreakTime;
+  int longBreakTime;
+  int countPeriods;
 
   pomodoroDescription pomDesc;
   PomodoroTimer pomTimer;
 
 
   _PomodoroState(this.title, this.icon){
+    pomodoroDefaultValues();
     pomDesc=new pomodoroDescription(periodTime, shortBreakTime, longBreakTime, countPeriods);
     pomTimer= new PomodoroTimer();
     updateVales(periodTime, shortBreakTime, longBreakTime, countPeriods);
   }
 
+
+  //set the default values for pomodoro
+  void pomodoroDefaultValues(){
+    periodTime=10;
+    shortBreakTime=4;
+    longBreakTime=3;
+    countPeriods=9;
+  }
 
   @override
   Widget build(BuildContext context) {
