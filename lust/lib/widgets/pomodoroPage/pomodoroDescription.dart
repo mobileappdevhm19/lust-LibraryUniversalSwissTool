fdd7b39b608a49132518eb9f11afc7c1e8c2cfa0
mike <lauterba@hm.edu>
2019-06-28 23:07:11 +0200

    first changes for pomodoroDescriptions incl. Tests

--- lust/lib/widgets/pomodoroPage/pomodoroDescription.dart
+++ lust/lib/widgets/pomodoroPage/pomodoroDescription.dart
@@ -1,352 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:lust/pages/pomodoroPage/pomodoroPage.dart';
-
 
 class pomodoroDescription extends StatelessWidget {
   int periodTime;
-  int oldPeriodTime;
-
   int shortBreakTime;
-  int oldShortBreakTime;
-
   int longBreakTime;
-  int oldLongBreakTime;
-
   int countPeriods;
-  int oldCountPeriods;
 
-  PomodoroState pomPage;
-
-  BuildContext context;
-
-
-  //Text for the tooltip-snackboxes
-  Color errorCol=Colors.red;
-  Color infoCol=Colors.black;
-
-  static String haveToClickOK="\n \n You have to click e.g. 'OK' on your keyboard to use your inputs.";
-  String breakSnackText="Here you can change the duration of short and long breaks."+haveToClickOK;
-
-  String periodSnackText="Here you can choose the duration of a learning period. \n\n"
-                          "You can also choose how many periods till a long break you want to learn"+haveToClickOK;
-  
-
-  TextEditingController  periodTimeController;
-  TextEditingController  periodCountController;
-
-  TextEditingController  shortBreakController;
-  TextEditingController  longBreakController;
-
-  pomodoroDescription(){
-    periodTimeController = new TextEditingController();
-    periodCountController = new TextEditingController();
-
-    shortBreakController = new TextEditingController();
-    longBreakController = new TextEditingController();
-  }
-
-  void setValues(int periodTime, int shortBreakTime, int longBreakTime, int countPeriods, PomodoroState pomPage){
+  pomodoroDescription(int periodTime, int shortBreakTime, int longBreakTime, int countPeriods){
     this.periodTime=periodTime;
     this.shortBreakTime=shortBreakTime;
     this.longBreakTime=longBreakTime;
     this.countPeriods=countPeriods;
-    setValuesToTextFields();
-    this.pomPage=pomPage;
-    updateValues(true); //to set old values
   }
+
+
   @override
   Widget build(BuildContext context) {
-    this.context=context;
-    const double margin=10;
-    double dividerHeight = 1.0;
-    double dividerIndent = 0.0;
-    Color dividerColor = Colors.black;
-
     return new Container(
-      margin: const EdgeInsets.only(left: margin, right: margin, top: margin, bottom: margin),
       child: Column(
         //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-
-            headlineWithWidget("Period",  Icons.help, periodSnackText, context),
+            new Row(
+              children: <Widget>[
+                Expanded(child: new Column(
+                    children: <Widget>[
+                      Text(
+                        "Period",
+                        textScaleFactor: 0.8,
+                        style: TextStyle(fontSize: 30.0, letterSpacing: 2.0),
+                        textAlign: TextAlign.center,
+                      ),
+                    ]),
+                ),
+              ],
+            ),
 
             new Row(
               children: <Widget>[
                Expanded(child: new Column(
                 children: <Widget>[
-                  TextField(
-                    decoration: new InputDecoration(labelText: "time [min]", ),
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black45,
-                        fontWeight: FontWeight.bold),
+                  new TextField(
+                    decoration: new InputDecoration(labelText: "time"),
                     keyboardType: TextInputType.number,
-                    controller: periodTimeController,
-                    onSubmitted: (text) {
-                      periodTime=int.parse(periodTimeController.text);
-                      updateValues();
-                    },
+
                   ),
                 ]),
                ),
-               VerticalDivider(),
-               Expanded(child: new Column(
+                Expanded(child: new Column(
                     children: <Widget>[
-                      TextField(
-                        decoration: new InputDecoration(labelText: "count", ),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black45,
-                            fontWeight: FontWeight.bold),
+                      new TextField(
+                        decoration: new InputDecoration(labelText: "count"),
                         keyboardType: TextInputType.number,
-                        controller: periodCountController,
-                        onSubmitted: (text) {
-                          countPeriods=int.parse(periodCountController.text);
-                          updateValues();
-                        },
+
                       ),
                     ]),
                 ),
 
+
               ],
             ),
-
-            new HorizontalDivider(),
-
-            headlineWithWidget("Break",  Icons.help, breakSnackText, context),
 
             new Row(
               children: <Widget>[
                 Expanded(child: new Column(
                     children: <Widget>[
-                      TextField(
-                        decoration: new InputDecoration(labelText: "short [min]", ),
+                      Text(
+                        "Break",
+                        textScaleFactor: 0.8,
+                        style: TextStyle(fontSize: 30.0, letterSpacing: 2.0),
                         textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black45,
-                            fontWeight: FontWeight.bold),
-                        keyboardType: TextInputType.number,
-                        controller: shortBreakController,
-                        onSubmitted: (text) {
-                          shortBreakTime=int.parse(shortBreakController.text);
-                          updateValues();
-                        },
-                      ),
-                    ]),
-                ),
-                VerticalDivider(),
-                Expanded(child: new Column(
-                    children: <Widget>[
-                      TextField(
-                        decoration: new InputDecoration(labelText: "long [min]", ),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black45,
-                            fontWeight: FontWeight.bold),
-                        keyboardType: TextInputType.number,
-                        controller: longBreakController,
-                        onSubmitted: (text) {
-                          longBreakTime=int.parse(longBreakController.text);
-                          updateValues();
-                        },
                       ),
                     ]),
                 ),
               ],
             ),
-            new HorizontalDivider(),
+
+            new Row(
+              children: <Widget>[
+                Expanded(child: new Column(
+                    children: <Widget>[
+                      new TextField(
+                        decoration: new InputDecoration(labelText: "short"),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ]),
+                ),
+                Expanded(child: new Column(
+                    children: <Widget>[
+                      new TextField(
+                        decoration: new InputDecoration(labelText: "long"),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ]),
+                ),
+              ],
+            ),
+            new SizedBox(
+              height: 10.0,
+              child: new Center(
+                child: new Container(
+                  margin: new EdgeInsetsDirectional.only(start: 1.0, end: 1.0),
+                  height: 5.0,
+                  color: Colors.red,
+                ),
+              ),
+            )
           ]),
     );
-  }
-
-  //setter for the text fields
-  void setValuesToTextFields(){
-    //print("in setValuesToTextFields $periodTime");
-    periodTimeController.text=periodTime.toString();
-    periodCountController.text=countPeriods.toString();
-
-    shortBreakController.text=shortBreakTime.toString();
-    longBreakController.text=longBreakTime.toString();
-  }
-
-
-  Row headlineWithWidget(String text, IconData icon, String snackText, BuildContext context){
-
-    return new Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            alignment: Alignment.topRight,
-            child: Text(
-              "$text",
-              textScaleFactor: 0.8,
-              style: TextStyle(fontSize: 30.0,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-        Expanded(
-          child: Container(
-          alignment: Alignment.topLeft,
-          child: IconButton(
-            iconSize: 30.0,
-            icon: Icon(icon),
-            //color: theme.iconTheme.color,
-            tooltip: 'Tooltip for $text',
-            onPressed: () {
-              printSnackBoc(snackText, infoCol);
-            },
-          ),
-        ),
-        ),
-      ],
-    );
-  }
-
-
-  void updateValues([bool init=false]){
-    bool ret=testInputs();
-    if(ret==false && init==false){ //set all to old values
-      setOldValues();
-    }
-    else{ //set new values
-      if(pomPage !=null){
-        pomPage.updateVales(periodTime, shortBreakTime, longBreakTime, countPeriods);
-      }
-    }
-
-    //set values to old value
-    oldPeriodTime=periodTime;
-    oldShortBreakTime=shortBreakTime;
-    oldLongBreakTime=longBreakTime;
-    oldCountPeriods=countPeriods;
-  }
-
-  void setOldValues(){
-    periodTime=oldPeriodTime;
-    shortBreakTime= oldShortBreakTime;
-    longBreakTime=oldLongBreakTime;
-    countPeriods=oldCountPeriods;
-
-    setValuesToTextFields();//set the values to the textFields
-  }
-
-  //snackbox when silly inputs
-  //true, when only warning/ info
-  //false, when error (use old value)
-  bool testInputs(){
-    String snackText;
-
-
-    //all error things
-
-    //cases, when variable <0
-      String varName="";
-      String oldVal="";
-
-    if(periodTime<=0){
-      varName="period time";
-      oldVal=pomPage.periodTime.toString();
-    }
-
-    if(countPeriods<=0){
-      varName="count of periods";
-      oldVal=pomPage.countPeriods.toString();
-    }
-
-    if(shortBreakTime<=0){
-      varName="short break time";
-      oldVal=pomPage.shortBreakTime.toString();
-    }
-
-    if(longBreakTime<=0){
-      varName="long break time";
-      oldVal=pomPage.longBreakTime.toString();
-    }
-
-
-      if(varName !=""){
-        snackText="Your Input for '"+varName+"' is <=0. \n"
-            "Pomodoro Timer will still use the old value: "+oldVal;
-        printSnackBoc(snackText, errorCol);
-        return false;
-      }
-
-      //info messages (multiple messages after another possible)
-    if(shortBreakTime>=periodTime || longBreakTime>=periodTime){
-      snackText="Breaktime is longer than your learning time! \n"
-          "You are so lazy";
-      printSnackBoc(snackText, infoCol);
-    }
-
-    if(shortBreakTime> longBreakTime){
-      snackText="short break time longer than long break Time \n"
-          "That is useless";
-      printSnackBoc(snackText, infoCol);
-    }
-
-    return true;
 
   }
 
-  void printSnackBoc(String text, Color color){
-    const dur = const Duration(seconds: 5);
-  if(this.context==null)return;
-    Scaffold.of(this.context).showSnackBar(SnackBar(
-        content: Text("$text"),
-      backgroundColor: color,
-      duration: dur,
-    ));
-  }
 }
-
-
-class emptyColumn extends StatelessWidget{
-
-  @override
-  Widget build(BuildContext context){
-    return new Column(
-      children: <Widget>[
-      Expanded(child: new Row(
-      children: <Widget>[
-        Text("hi"),
-      ])),
-    ]);
-  }
-}
-
-
-class VerticalDivider extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      height: 30.0,
-      width: 1.0,
-      color: Colors.black,
-      margin: const EdgeInsets.only(left: 10.0, right: 10.0),
-    );
-  }
-}
-
-
-class HorizontalDivider extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new SizedBox(
-        height: 10.0,
-        child: new Center(
-        child: new Container(
-        margin: new EdgeInsetsDirectional.only(start: 1.0, end: 1.0),
-    height: 5.0,
-    color: Colors.black,
-    ),
-    ));
-  }
-}
-
--- lust/test/widget_tests/pomodoroPage_tests/pomodoroDescription_test.dart
+++ lust/test/widget_tests/pomodoroPage_tests/pomodoroDescription_test.dart
@@ -2,25 +2,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:lust/main.dart';
 import 'package:flutter/material.dart';
 import '../../testHelper.dart';
-import 'package:lust/pages/pomodoroPage/pomodoroPage.dart';
+import 'package:lust/pages/pomodoroPage.dart';
 import 'package:lust/widgets/pomodoroPage/pomodoroDescription.dart';
 
 
-pomodoroDescription pomDesc;
 
-int periodTime=10;
-int shortBreakTime=4;
-int longBreakTime=3;
-int countPeriods=9;
-
-PomodoroState pomPage;
 
 void main() {
-  pomPage=new PomodoroState();
-  pomDesc=new pomodoroDescription();
-
-  pomDesc.setValues(periodTime, shortBreakTime, longBreakTime, countPeriods, pomPage);
-
   testWidgets('pomodoroDesc headline tests', (WidgetTester tester) async {
     await tester.pumpWidget(TestHelper.buildWidget(PomodoroPage()));
     checkHeadlines(tester);
@@ -31,28 +19,8 @@ void main() {
     await tester.pumpWidget(TestHelper.buildWidget(PomodoroPage()));
     checkTextFieldTexts(tester);
   });
-
-  testWidgets('pomodoroDesc checkInitialValues', (WidgetTester tester) async {
-    await tester.pumpWidget(TestHelper.buildWidget(PomodoroPage()));
-    checkInitialValues(tester);
-  });
-
-  testWidgets('pomodoroDesc checkInitialTextFieldsValues', (WidgetTester tester) async {
-    await tester.pumpWidget(TestHelper.buildWidget(PomodoroPage()));
-    checkInitialTextFieldsValues(tester);
-  });
-
-  testWidgets('pomodoroDesc check Old Values', (WidgetTester tester) async {
-    await tester.pumpWidget(TestHelper.buildWidget(PomodoroPage()));
-    checkOldValues(tester);
-  });
-
-  testWidgets('pomodoroDesc test inputs', (WidgetTester tester) async {
-    await tester.pumpWidget(TestHelper.buildWidget(PomodoroPage()));
-    testInputs(tester);
-  });
-
 }
+
 
 
 void checkHeadlines(WidgetTester tester){
@@ -64,78 +32,15 @@ void checkHeadlines(WidgetTester tester){
 }
 
 void checkTextFieldTexts(WidgetTester tester){
-  final periodTimeFinder = find.text("time [min]");
+  final periodTimeFinder = find.text("time");
   expect(periodTimeFinder, findsOneWidget);
 
   final periodCountFinder = find.text("count");
   expect(periodCountFinder, findsOneWidget);
 
-  final shortBreakFinder = find.text("short [min]");
+  final shortBreakFinder = find.text("short");
   expect(shortBreakFinder, findsOneWidget);
 
-  final longBreakFinder = find.text("long [min]");
+  final longBreakFinder = find.text("long");
   expect(longBreakFinder, findsOneWidget);
-}
-
-void checkInitialValues(WidgetTester tester){
-  pomDesc.setValues(periodTime, shortBreakTime, longBreakTime, countPeriods, pomPage);
-
-  expect(periodTime, pomDesc.periodTime);
-  expect(shortBreakTime, pomDesc.shortBreakTime);
-  expect(longBreakTime, pomDesc.longBreakTime);
-  expect(countPeriods, pomDesc.countPeriods);
-  expect(pomPage, pomDesc.pomPage);
-}
-
-void checkInitialTextFieldsValues(WidgetTester tester){
-  pomDesc.setValuesToTextFields();
-
-  expect(pomDesc.periodTimeController.text, pomDesc.periodTime.toString());
-  expect(pomDesc.shortBreakController.text, pomDesc.shortBreakTime.toString());
-  expect(pomDesc.longBreakController.text, pomDesc.longBreakTime.toString());
-  expect(pomDesc.periodCountController.text, pomDesc.countPeriods.toString());
-
-  pomDesc.updateValues();
-}
-
-
-void checkOldValues(WidgetTester tester){
-  int oldPeriodTime=pomDesc.oldPeriodTime;
-  int oldShortBreakTime=pomDesc.oldShortBreakTime;
-  int oldLongBreakTime=pomDesc.oldLongBreakTime;
-  int oldCountPeriods=pomDesc.oldCountPeriods;
-
-  pomDesc.setOldValues();
-
-  expect(pomDesc.periodTime, oldPeriodTime);
-  expect(pomDesc.shortBreakTime, oldShortBreakTime);
-  expect(pomDesc.longBreakTime, oldLongBreakTime);
-  expect(pomDesc.countPeriods, oldCountPeriods);
-}
-
-void testInputs(WidgetTester tester){
-  pomDesc.periodTime=0;
-  expect(pomDesc.testInputs(), false);
-  pomDesc.periodTime=1;
-
-  pomDesc.shortBreakTime=0;
-  expect(pomDesc.testInputs(), false);
-  pomDesc.shortBreakTime=1;
-
-  pomDesc.longBreakTime=0;
-  expect(pomDesc.testInputs(), false);
-  pomDesc.longBreakTime=1;
-
-  pomDesc.countPeriods=0;
-  expect(pomDesc.testInputs(), false);
-  pomDesc.countPeriods=1;
-
-  //"Sily inputs
-  pomDesc.shortBreakTime=2;
-  pomDesc.longBreakTime=1;
-  expect(pomDesc.testInputs(), true);
-
-  pomDesc.shortBreakTime=2;
-  pomDesc.periodTime=1;
-  expect(pomDesc.testInputs(), true);
 }
