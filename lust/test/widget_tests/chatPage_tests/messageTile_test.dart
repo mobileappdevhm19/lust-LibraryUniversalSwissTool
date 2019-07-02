6f909ae1f17a90a567bd04ca052f9b31cbc3951b
andre <brauni.andre@gmail.com>
2019-07-02 20:30:05 +0200

    add dummy for message tile test

---
+++ lust/test/widget_tests/chatPage_tests/messageTile_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../testHelper.dart';
+
+import 'package:lust/widgets/chatPage/messageTile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  testWidgets('The chat page has a textfield at the bottom', (WidgetTester tester) async {
+    /*
+    final DocumentSnapshot _document = DocumentSnapshot();
+    final String _userID = "userID";
+    MessageTile tile = MessageTile(_document, _userID)
+
+    // Create the Widget tell the tester to build it
+    await tester.pumpWidget(TestHelper.buildWidget(page.pageObject));
+
+    // Create our Finders
+    final titleFinder = find.text('Tutor Chat');
+    final textFieldFinder = find.byType(TextField);
+    final sendButtonFinder = find.byIcon(Icons.send);
+    final hintFinder = find.text('Send a message');
+
+    // Check if the title there
+    expect(titleFinder, findsOneWidget);
+
+    // Check if the Textfield is there and has the hint
+    expect(textFieldFinder, findsOneWidget);
+    expect(hintFinder, findsOneWidget);
+
+    // Check if the send button is there
+    expect(sendButtonFinder, findsOneWidget);
+
+    */
+  });
+}
