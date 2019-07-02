b5315f8f6884f907ec8d6f3a75b7e5039cb2be46
Felix Strobel <mail@felixstrobel.de>
2019-07-02 12:02:36 +0200

    refactoring library to create it from firebase

--- lust/lib/models/library.dart
+++ lust/lib/models/library.dart
@@ -1,130 +1,75 @@
-import 'package:lust/models/percentPerHour.dart';
-import 'package:lust/models/trend.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Library {
 // interface
-  double latitude;
-  double longitude;
+  GeoPoint _location;
 
   DateTime _openingTimeToday;
   DateTime _closingTimeToday;
-
+  String _address;
   int _currentFilling;
-  int _maxCapacity;
+  Map<String, int> _occupancyMap = Map();
+  String _name;
+  int _totalSeats;
 
-  List<PercentPerHour> _occupancyPerHour;
 
-  Library({this.latitude, this.longitude});
+  set occupancyMap(Map<String, int> value) {
+    _occupancyMap = value;
+  }
 
-  /// creates a library with sample data
-  Library.withSampleData() {
-    // Set the Opening hour to today 8:00 and closing to 24
-    _closingTimeToday = DateTime(
-        DateTime
-            .now()
-            .year, DateTime
+  DateTime get openingTimeToday => _openingTimeToday;
+
+  Map<String, int> get occupancyMap => _occupancyMap;
+
+  GeoPoint get location => _location;
+
+  String get name => _name;
+
+  DateTime get closingTimeToday => _closingTimeToday;
+
+  String get address => _address;
+
+  int get currentFilling => _currentFilling;
+
+  int get totalSeats => _totalSeats;
+
+  Library() {
+    _occupancyMap["8"] = 0;
+    _occupancyMap["10"] = 0;
+    _occupancyMap["12"] = 0;
+    _occupancyMap["14"] = 0;
+    _occupancyMap["16"] = 0;
+    _occupancyMap["18"] = 0;
+    _occupancyMap["20"] = 0;
+    _occupancyMap["22"] = 0;
+
+    _openingTimeToday = DateTime(DateTime
+        .now()
+        .year, DateTime
         .now()
         .month, DateTime
         .now()
-        .day, 24);
-    _openingTimeToday = DateTime(
-        DateTime
-            .now()
-            .year, DateTime
+        .day, 0, 1);
+    _closingTimeToday = DateTime(DateTime
+        .now()
+        .year, DateTime
         .now()
         .month, DateTime
         .now()
-        .day, 8);
-
-    _occupancyPerHour = [
-      PercentPerHour(17, "8"),
-      PercentPerHour(35, "10"),
-      PercentPerHour(13, "12"),
-      PercentPerHour(7, "14"),
-      PercentPerHour(22, "16"),
-      PercentPerHour(50, "18"),
-      PercentPerHour(87, "20"),
-      PercentPerHour(69, "22"),
-    ];
-
-    _currentFilling = 60;
-    _maxCapacity = 120;
+        .day, 23, 59);
   }
 
-  /// Gives you the opening time today.
-  /// Checks the current date and if the day is not today it ask of the new
-  /// opening time for the current day
-  DateTime getOpeningTimeToday() {
-    // TODO get the time from firebase
-    return _openingTimeToday;
+  Library.withLocation(this._location);
+
+  void setDataFromMap(Map<String, dynamic> map) {
+    this._address = map["address"];
+    this._openingTimeToday =
+        DateTime.fromMillisecondsSinceEpoch(map["openingTime"].seconds * 1000);
+    this._closingTimeToday =
+        DateTime.fromMillisecondsSinceEpoch(map["closingTime"].seconds * 1000);
+    this._location = map["location"];
+    this._name = map["name"];
+    this._totalSeats = map["totalseats"];
   }
 
-  /// Gives you the closing time today
-  /// checks the current date and if the day is not today it ask of the new
-  /// opening time for the current day
-  DateTime getClosingTimeToday() {
-    // TODO get the time from firebase
-    return _closingTimeToday;
-  }
-
-  /// Uses DateTime.now() to calculate the opening and closing hours and
-  /// gives out an array of opening ours between
-  /// the opening our and the closing hour.
-  getOccupancyPercentPerHour() {
-    int durationHours =
-        DateTime
-            .now()
-            .difference(getOpeningTimeToday())
-            .inHours;
-
-    for (int index = 0; index < _occupancyPerHour.length; index++) {
-      if (index > durationHours ~/ 2)
-        _occupancyPerHour
-            .elementAt(index)
-            .percent = 0;
-    }
-    return _occupancyPerHour;
-  }
-
-  // Calculate the trend by comparing the current occupancy percent with the last
-  Trend getEstimatedTrend() {
-    int threshold = 3;
-
-    Trend trend;
-    int durationHours =
-        DateTime
-            .now()
-            .difference(getOpeningTimeToday())
-            .inHours;
-
-    if (durationHours ~/ 2 <= 0)
-      trend = Trend.rising;
-    else {
-      int previousPercent =
-          _occupancyPerHour
-              .elementAt(durationHours ~/ 2 - 1)
-              .percent;
-      int currentPercent =
-          _occupancyPerHour
-              .elementAt(durationHours ~/ 2)
-              .percent;
-
-      int trendIndex = currentPercent - previousPercent;
-
-      trendIndex < -threshold
-          ? trend = Trend.falling
-          : trendIndex > threshold
-          ? trend = Trend.rising
-          : trend = Trend.leveling;
-    }
-    return trend;
-  }
-
-  int getCurrentFilling() {
-    return _currentFilling;
-  }
-
-  int getMaxCapacity() {
-    return _maxCapacity;
-  }
 }
