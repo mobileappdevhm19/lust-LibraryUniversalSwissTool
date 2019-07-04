f8b9405a2e83554d6acfede938be2b9f1331526d
Felix Strobel <mail@felixstrobel.de>
2019-07-04 13:23:58 +0200

    only widgets tests

--- lust/test/utils_tests/auth_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:lust/utils/autenthicationAPI.dart';
-
-void main() {
-  test("ConstructorTest", () {
-    final auth = Auth();
-  });
-}
--- lust/test/utils_tests/locationAPI_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:lust/utils/locationAPI.dart';
-
-void main() {
-  test("ConstructorTest", () {
-    final auth = LocationAPI();
-  });
-}
