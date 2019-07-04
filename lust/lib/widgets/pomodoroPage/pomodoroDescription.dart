2b07cdb56a79cc150dc78d65421cd60db5b1b9f5
mike <lauterba@hm.edu>
2019-07-04 13:08:50 +0200

    after rebasing

--- lust/lib/widgets/pomodoroPage/pomodoroDescription.dart
+++ lust/lib/widgets/pomodoroPage/pomodoroDescription.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lust/pages/pomodoroPage.dart';
+import 'package:lust/pages/pomodoroPage/pomodoroPage.dart';
 
 
 class pomodoroDescription extends StatelessWidget {
